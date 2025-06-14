8a02b6f9d037bf4fb07017ae9eae73f3876a001c
rrlc5 <lyl192122@gmail.com>
2025-06-14 12:49:41 +0300

    Add lib/main.dart with basic app code

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Super Game',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('لعبة Super'),
+        ),
+        body: const Center(
+          child: Text('مرحباً بك في لعبة Super!'),
+        ),
+      ),
+    );
+  }
+}
